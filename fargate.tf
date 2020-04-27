671bc6a46b7ca32c468f7dedef7c8f2c19370b67
kert <kaidokert@gmail.com>
2020-04-27 16:37:44 -0700

    Working EFS mount

--- fargate.tf
+++ fargate.tf
@@ -1,34 +1,67 @@
-variable "aws_region" {}
-variable "cidr1" {}
-variable "cidr2" {}
+variable "tf_region" {}
+variable "tf_cidr" {}
 
 locals {
-  family = "tf-run"
+  family = "terraform-task-definition"
 }
 
 provider "aws" {
-  region = var.aws_region
-}
-resource "aws_vpc" "test_vpc" {
-  cidr_block = "${var.cidr1}/16"
-}
-resource "aws_subnet" "sub1" {
-  vpc_id = aws_vpc.test_vpc.id
-  cidr_block = "${var.cidr1}/24"
-}
-resource "aws_subnet" "sub2" {
-  vpc_id = aws_vpc.test_vpc.id
-  cidr_block = "${var.cidr2}/24"
+  region = var.tf_region
 }
 
-resource "aws_security_group" "elb_sg" {
+resource "aws_iam_role" "test_exec_role" {
+  name = "ecsTaskExecutionRole"
+  force_detach_policies  = true
+  assume_role_policy = jsonencode({
+    Version   = "2008-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+      Sid = ""
+    }]
+  })
+}
+
+resource "aws_iam_policy_attachment" "test-attach" {
+  name = "tf-ECSTaskExecutionRolePolicy"
+  roles      = [aws_iam_role.test_exec_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_vpc" "test_vpc" {
+  cidr_block = "${var.tf_cidr}/16"
+  enable_dns_hostnames = true
+}
+
+resource "aws_internet_gateway" "test_gateway" {
   vpc_id = aws_vpc.test_vpc.id
-  name = "terraform-fargate-sg"
+}
+
+resource "aws_default_route_table" "test_route_table" {
+  # vpc_id = aws_vpc.test_vpc.id
+  default_route_table_id = aws_vpc.test_vpc.default_route_table_id
+  route  {
+    cidr_block                = "0.0.0.0/0"
+    gateway_id                = aws_internet_gateway.test_gateway.id
+  }
+}
+
+resource "aws_subnet" "test_subnet" {
+  vpc_id = aws_vpc.test_vpc.id
+  cidr_block = "${var.tf_cidr}/24"
+}
+
+resource "aws_security_group" "test_security_group" {
+  vpc_id = aws_vpc.test_vpc.id
+  name = "terraform-security-group"
   revoke_rules_on_delete = true
   egress {
     cidr_blocks      = [ "0.0.0.0/0"]
     protocol         = "-1"
-    from_port        = 0    
+    from_port        = 0
     to_port          = 0
   }
   ingress {
@@ -37,50 +70,107 @@ resource "aws_security_group" "elb_sg" {
     from_port        = 80
     to_port          = 80
   }
+  ingress {
+    cidr_blocks      = [ "0.0.0.0/0"]
+    protocol = "tcp"
+    from_port        = 2049
+    to_port          = 2049
+  }
 }
 
-resource "aws_ecs_task_definition" "example" {
+resource "aws_default_security_group" "test_security_group" {
+  vpc_id = aws_vpc.test_vpc.id
+  ingress {
+    security_groups = [ aws_security_group.test_security_group.id ]
+    protocol = "-1"
+    from_port        = 0
+    to_port          = 0
+  }
+}
+
+resource "aws_cloudwatch_log_group" "test_log_group" {
+  name =  "/ecs/${local.family}"
+}
+
+resource "aws_efs_file_system" "test_efs_file_system" {
+  creation_token = "test_efs"
+  depends_on = [ aws_subnet.test_subnet, aws_security_group.test_security_group ]
+}
+
+resource "aws_efs_mount_target" "test_mount_target" {
+    file_system_id = aws_efs_file_system.test_efs_file_system.id
+    subnet_id = aws_subnet.test_subnet.id
+    security_groups = [ aws_security_group.test_security_group.id ]
+}
+
+resource "aws_ecs_task_definition" "test_ecs_task_definition" {
   family = local.family
+  cpu = "256"
   memory = "512"
   network_mode  = "awsvpc"
-  cpu = "256"
-  container_definitions = jsonencode([
-    {
-      essential = true
-      name = "sample-app"
-      image = "httpd:2.4"
-      command = [
-        "/bin/sh -c \"echo '<html><body><h1>Hello</h1></body></html>' > /usr/local/apache2/htdocs/index.html && httpd-foreground\"",
-      ]
-      portMappings = [{
-        containerPort = 80
-        hostPort      = 80
-        protocol      = "tcp"
-      }]
-      environment       = []
-      mountPoints       = []
-      volumesFrom       = []
-      logConfiguration  = {
-        logDriver = "awslogs"
-        options   = {
-            awslogs-group         = "/ecs/${local.family}"
-            awslogs-region        = var.aws_region
-          }
+  requires_compatibilities = [ "FARGATE" ]
+  execution_role_arn = aws_iam_role.test_exec_role.arn
+  container_definitions = jsonencode([{
+    essential = true
+    name = "terraform-app-container"
+    image = "httpd:2.4"
+    entryPoint = [ "sh", "-c"]
+    command = [
+      "/bin/sh -c \"df -h /mnt/efs > /usr/local/apache2/htdocs/index.html && httpd-foreground\""
+    ]
+    portMappings = [{
+      containerPort = 80
+      hostPort      = 80
+      protocol      = "tcp"
+    }]
+    environment       = [{
+      name = "VAR1"
+      value = "VALUE1"
+    }]
+    mountPoints       = [
+      {
+        containerpath = "/mnt/efs"
+        sourceVolume = "service-storage"
       }
+    ]
+    logConfiguration  = {
+      logDriver = "awslogs"
+      options   = {
+          awslogs-group         = aws_cloudwatch_log_group.test_log_group.id
+          awslogs-region        = var.tf_region
+          awslogs-stream-prefix = "ecs"
+        }
     }
-  ])
+  }])
+  volume {
+    name = "service-storage"
+    efs_volume_configuration {
+      file_system_id = aws_efs_file_system.test_efs_file_system.id
+    }
+  }
+  depends_on = [
+    aws_efs_mount_target.test_mount_target
+  ]
 }
 
-resource "aws_ecs_service" "imported" {
-  name = "sample-app-service"
+resource "aws_ecs_cluster" "test_cluster" {
+  name = "terraform-cluster"
+  setting {
+      name  = "containerInsights"
+      value = "enabled"
+  }
+ }
+
+resource "aws_ecs_service" "test_ecs_service" {
+  name = "terraform-app-service"
+  platform_version = "1.4.0"
   launch_type = "FARGATE"
-  task_definition = aws_ecs_task_definition.example.arn
+  cluster = aws_ecs_cluster.test_cluster.id
+  task_definition = aws_ecs_task_definition.test_ecs_task_definition.arn
   desired_count = 1
   network_configuration {
       assign_public_ip = true
-      security_groups = [ aws_security_group.elb_sg.id ]
-      subnets = [
-        aws_subnet.sub1.id, aws_subnet.sub2.id
-      ]
+      security_groups = [ aws_security_group.test_security_group.id ]
+      subnets = [ aws_subnet.test_subnet.id ]
   }
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,3 +1,2 @@
-aws_region = "us-west-2"
-cidr1 = "10.0.1.0"
-cidr2 = "10.0.2.0"
+tf_region = "us-west-1"
+tf_cidr = "10.0.0.0"
